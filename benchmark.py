f8c14f6ac85bf2f62e581ee343c63cd74c396799
Ryosuke Ito <rito.0305@gmail.com>
2018-10-06 12:06:29 +0900

    pep8

--- benchmark.py
+++ benchmark.py
@@ -4,12 +4,14 @@ from string import ascii_letters
 from timeit import timeit
 from random import choice
 
-BYTES = ''.join(choice(ascii_letters) for i in range(1024))
+BYTES = ''.join(choice(ascii_letters) for _ in range(1024))
 NUMBER = 50000
 
 if __name__ == '__main__':
-    arc4 = timeit('arc4.ARC4("key").encrypt("{}")'.format(BYTES), 'import arc4', number=NUMBER)
-    pycrypto = timeit('ARC4.new("key").encrypt("{}")'.format(BYTES), 'import Crypto.Cipher.ARC4 as ARC4', number=NUMBER)
+    arc4 = timeit('arc4.ARC4("key").encrypt("{}")'.format(BYTES),
+                  'import arc4', number=NUMBER)
+    pycrypto = timeit('ARC4.new("key").encrypt("{}")'.format(BYTES),
+                      'import Crypto.Cipher.ARC4 as ARC4', number=NUMBER)
     rc4 = timeit('rc4.rc4("key", "bytes")', 'import rc4', number=NUMBER)
     print('arc4: {}'.format(arc4))
     print('pycrypto: {}'.format(pycrypto))
