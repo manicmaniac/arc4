e49ed30c7665fad62a25cd0d07b97b77c15b6ff3
Ryosuke Ito <rito.0305@gmail.com>
2018-10-08 23:40:44 +0900

    add classifiers to setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,7 @@
-from distutils.core import Extension, setup
+try:
+    from setuptools import Extension, setup
+except ImportError:
+    from distutils.core import Extension, setup
 
 setup(
     name='arc4',
@@ -11,5 +14,18 @@ setup(
     author_email='rito.0305@gmail.com',
     ext_modules=[
         Extension('arc4', sources=['arc4.c'], extra_compile_args=['-O3'])
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: Unix',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: MacOS :: MacOS X',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Topic :: Security :: Cryptography',
     ],
 )
