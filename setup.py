eebe741a7a4d5ae07f51c9dc26ea7fa4a9465599
Ryosuke Ito <rito.0305@gmail.com>
2018-10-08 23:46:42 +0900

    improve setup.py and MANIFEST.in

--- setup.py
+++ setup.py
@@ -3,6 +3,11 @@ try:
 except ImportError:
     from distutils.core import Extension, setup
 
+
+with open('README.rst') as f:
+    long_description = f.read()
+
+
 setup(
     name='arc4',
     version='0.0.1',
@@ -10,8 +15,10 @@ setup(
         'A small and insanely fast ARCFOUR (RC4) cipher implementation ' +
         'of Python'
     ),
+    long_description=long_description,
     author='Ryosuke Ito',
     author_email='rito.0305@gmail.com',
+    url='https://github.com/manicmaniac/arc4',
     ext_modules=[
         Extension('arc4', sources=['arc4.c'], extra_compile_args=['-O3'])
     ],
