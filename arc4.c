3d8393f6ad41fa37a0062dd7d199189f020340f0
Ryosuke Ito <rito.0305@gmail.com>
2020-10-21 01:11:56 +0900

    Declare same types at once

--- arc4.c
+++ arc4.c
@@ -2,9 +2,7 @@
 #include <Python.h>
 
 typedef struct arc4_state {
-    unsigned char x;
-    unsigned char y;
-    unsigned char s[256];
+    unsigned char x, y, s[256];
 } arc4_state;
 
 static void arc4_init(arc4_state *state, const unsigned char *key, Py_ssize_t keylen) {
@@ -26,12 +24,8 @@ static void arc4_init(arc4_state *state, const unsigned char *key, Py_ssize_t ke
 }
 
 static void arc4_crypt(arc4_state *state, unsigned char *buf, Py_ssize_t buflen) {
-    unsigned char x;
-    unsigned char y;
-    unsigned char *s;
+    unsigned char x, y, *s, sx, sy;
     Py_ssize_t i;
-    unsigned char sx;
-    unsigned char sy;
 
     x = state->x;
     y = state->y;
